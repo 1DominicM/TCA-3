5d6c829eaf7956dfd800ba993c93f03b72a86712
Dominic <125dmiguelcruz@frhsd.com>
2024-03-26 09:46:25 -0400

    first commit

---
+++ js/java.js
@@ -0,0 +1,29 @@
+function updateOne(){
+    document.getElementById('output').innerHTML = "Bees play a vital role in ecosystems and agriculture, making them indispensable to human life. Bees are incredibly important creatures in our world, without bees, the world would face significant challenges in maintaining biodiversity and ensuring food security.";
+    document.getElementById('img').src="img/b1-removebg-preview.png";
+    document.getElementById('be2').innerHTML = "SAVE THE BEES";
+
+}
+function updateTwo(){
+    document.getElementById('output').innerHTML = "Bees matter immensely due to their crucial role as pollinators. They facilitate the reproduction of a vast array of plants, including many crops that humans rely on for food. Without bees, the ecosystem would suffer from reduced biodiversity, and agricultural productivity would decline significantly, leading to food shortages and economic instability. Beyond their agricultural importance, bees contribute to the overall health of ecosystems and support various industries, such as honey production and pharmaceuticals. Protecting bees is essential for maintaining ecological balance, ensuring food security, and sustaining human livelihoods.";
+    document.getElementById('img').src="img/b2-removebg-preview.png";
+    document.getElementById('be2').innerHTML = "Why do Bees Matter?";
+}
+function updateThree(){
+    document.getElementById('output').innerHTML = "Pesticides pose a severe threat to bee populations and the ecosystems they support. These chemicals, designed to kill pests, often have unintended consequences on non-target organisms like bees. Pesticides can harm bees directly by poisoning them or indirectly by contaminating their food sources and habitat. Exposure to pesticides weakens bees' immune systems, making them more susceptible to diseases and parasites. Additionally, pesticides can disrupt bees' navigation abilities, impairing their foraging and communication behaviors. To help bee populations and ecosystem health, it is crucial to reduce the use of harmful pesticides and adopt sustainable agricultural practices that aren't harmful.";
+    document.getElementById('img').src="img/pesticides_og.jpg";
+    document.getElementById('be2').innerHTML = "What Threatens Bees?";
+}
+function updateFour(){
+    document.getElementById('output').innerHTML = "A young German physicist, Hans von Ohain, worked for Ernst Heinkel, specializing in advanced engines, to develop the world's first jet plane, the experimental Heinkel He 178. I was the first jet-powered aircraft, it flew on Aug. 27, 1939, nearly two years before its British equivalent, the Gloster E. 28/39";
+    document.getElementById('img').src="img/plane1.webp";
+}
+function updateFive(){
+    document.getElementById('output').innerHTML = "The Mariner 2 spacecraft was launched by NASA from Cape Canaveral on August 27, 1962, and passed as close as 34,773 kilometers (21,607 mi) to Venus on December 14, 1962.";
+    document.getElementById('img').src="img/launch1.webp";
+}
+function updateSix(){
+    document.getElementById('output').innerHTML = "On August 27, 1833, Krakatoa erupts in a series of violent explosions. Causing tidal waves up to 120 ft. high.";
+    document.getElementById('img').src="img/volc1.jpg";
+}
+
